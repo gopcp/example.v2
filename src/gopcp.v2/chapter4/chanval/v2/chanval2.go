389f3235618dabe93bf21e579f9476cdcc2d241f
Max Qiu <yueyiqiu178@gmail.com>
2023-11-07 20:25:46 +0800

    優化chanval2.go

--- src/gopcp.v2/chapter4/chanval/v2/chanval2.go
+++ src/gopcp.v2/chapter4/chanval/v2/chanval2.go
@@ -2,42 +2,71 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 // Counter 代表计数器的类型。
 type Counter struct {
-	count int
+	pcount int
 }
 
-var mapChan = make(chan map[string]Counter, 1)
+var mapChan = make(chan map[string]*Counter, 1)
 
 func main() {
-	syncChan := make(chan struct{}, 2)
+	waitChan := make(chan struct{}, 2)
+	next := make(chan struct{})
+
 	go func() { // 用于演示接收操作。
 		for {
+
 			if elem, ok := <-mapChan; ok {
-				counter := elem["count"]
-				counter.count++
+
+				stop, ok := <-next
+				if !ok {
+					break
+				}
+
+				counter := elem["pcount"]
+				counter.pcount++
+
+				next <- stop
+
 			} else {
 				break
 			}
+
 		}
 		fmt.Println("Stopped. [receiver]")
-		syncChan <- struct{}{}
+		waitChan <- struct{}{}
 	}()
 	go func() { // 用于演示发送操作。
-		countMap := map[string]Counter{
-			"count": Counter{},
+		countMap := map[string]*Counter{
+			"pcount": &Counter{},
 		}
 		for i := 0; i < 5; i++ {
+
+			stop := <-next
+
 			mapChan <- countMap
-			time.Sleep(time.Millisecond)
 			fmt.Printf("The count map: %v. [sender]\n", countMap)
+
+			if i == 4 {
+				close(next)
+				break
+			}
+			next <- stop
 		}
 		close(mapChan)
-		syncChan <- struct{}{}
+		waitChan <- struct{}{}
 	}()
-	<-syncChan
-	<-syncChan
+
+	next <- struct{}{}
+
+	<-waitChan
+	<-waitChan
+}
+
+func (counter *Counter) String() string {
+
+	return fmt.Sprintf("{count: %d}", counter.pcount)
+
 }
