6766a04809a80b5ea3fcbeebb2a4dbc7e3c64979
Max Qiu <yueyiqiu178@gmail.com>
2023-11-08 20:19:38 +0800

    improve chan0cap.go

--- src/gopcp.v2/chapter4/chan0cap/chan0cap.go
+++ src/gopcp.v2/chapter4/chan0cap/chan0cap.go
@@ -8,38 +8,25 @@ import (
 func main() {
 	sendingInterval := time.Second
 	receptionInterval := time.Second * 2
-	intChan := make(chan int, 0)
+	intChan := make(chan int)
 	go func() {
-		var ts0, ts1 int64
 		for i := 1; i <= 5; i++ {
 			intChan <- i
-			ts1 = time.Now().Unix()
-			if ts0 == 0 {
-				fmt.Println("Sent:", i)
-			} else {
-				fmt.Printf("Sent: %d [interval: %d s]\n", i, ts1-ts0)
-			}
-			ts0 = time.Now().Unix()
+			fmt.Println("send[", i, "]:   ", time.Now())
 			time.Sleep(sendingInterval)
 		}
 		close(intChan)
 	}()
-	var ts0, ts1 int64
 Loop:
 	for {
 		select {
 		case v, ok := <-intChan:
+
 			if !ok {
 				break Loop
 			}
-			ts1 = time.Now().Unix()
-			if ts0 == 0 {
-				fmt.Println("Received:", v)
-			} else {
-				fmt.Printf("Received: %d [interval: %d s]\n", v, ts1-ts0)
-			}
+			fmt.Println("receive[", v, "]:", time.Now())
 		}
-		ts0 = time.Now().Unix()
 		time.Sleep(receptionInterval)
 	}
 	fmt.Println("End.")
