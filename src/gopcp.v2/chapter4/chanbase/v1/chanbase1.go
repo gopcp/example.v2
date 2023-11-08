556a60c2f30148631cbd7059a9eae184cfb05f4d
Max Qiu <yueyiqiu178@gmail.com>
2023-11-08 22:28:40 +0800

    improve again chanbase1.go

--- src/gopcp.v2/chapter4/chanbase/v1/chanbase1.go
+++ src/gopcp.v2/chapter4/chanbase/v1/chanbase1.go
@@ -2,36 +2,43 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 var strChan = make(chan string, 3)
 
 var goChan = make(chan string)
-var go2Chan = make(chan string)
-var ptr bool = true
+var flag = make(chan string, 1)
+var ptr bool = false
 
 func main() {
 	syncChan1 := make(chan struct{}, 1)
 	syncChan2 := make(chan struct{}, 2)
+	flag <- "S"
 	go func() { // 用于演示接收操作。
-		i := 0
 		<-syncChan1
-		fmt.Println("Received a sync signal and wait a second... [receiver]")
-		//time.Sleep(time.Second)
+		i := 0
+		fmt.Println("Receiver Begin... [receiver]")
 		for {
-			//time.Sleep(time.Second)
-			if elem, ok := <-strChan; ok {
-				fmt.Println("Received:", elem, "[receiver]")
-				i++
-				if i == 3 {
-					i = 0
-					goChan <- "GO"
-					<-go2Chan
-				}
 
+			fValue := <-flag
+			//time.Sleep(time.Second)
+			if fValue == "R" {
+				if elem, ok := <-strChan; ok {
+					fmt.Println("Received:", elem, "[receiver]")
+					i++
+					if i == 3 {
+						i = 0
+						flag <- "S"
+						goChan <- "GO"
+					} else {
+						flag <- "R"
+					}
+
+				} else {
+					break
+				}
 			} else {
-				break
+				flag <- "S"
 			}
 		}
 		fmt.Println("Stopped. [receiver]")
@@ -39,27 +46,39 @@ func main() {
 	}()
 	go func() { // 用于演示发送操作。
 		i := 0
-		for _, elem := range []string{"a", "b", "c", "d", "e", "f", "g", "h"} {
-			time.Sleep(time.Second)
-			strChan <- elem
-			fmt.Println("Sent:", elem, "[sender]")
-			i++
-			if i == 3 {
-				if ptr {
-					syncChan1 <- struct{}{}
-					ptr = false
-				}
-				fmt.Println("Sent a sync signal. [sender]")
-				i = 0
+		letterSlice := []string{"a", "b", "c", "d", "e", "f", "g", "h"}
+		fmt.Println("Send Begin... [sender]")
+		for index, elem := range letterSlice {
+			//time.Sleep(time.Second)
 
-				<-goChan
-				go2Chan <- "GO"
+			fValue := <-flag
+
+			if fValue == "S" {
+				strChan <- elem
+				fmt.Println("Sent:", elem, "[sender]")
+				i++
+
+				if index == (len(letterSlice) - 1) {
+					flag <- "R"
+				} else if i == 3 {
+					fmt.Println("Sent a sync signal. [sender]")
+					i = 0
+					if !ptr {
+						syncChan1 <- struct{}{}
+						ptr = true
+					}
+					flag <- "R"
+					<-goChan
+				} else {
+					flag <- "S"
+				}
+			} else {
+				flag <- "R"
 			}
 		}
-		fmt.Println("Wait 2 seconds... [sender]")
-		//time.Sleep(time.Second * 2)
 		close(strChan)
 		syncChan2 <- struct{}{}
+		fmt.Println("Stopped. [sender]")
 	}()
 	<-syncChan2
 	<-syncChan2
